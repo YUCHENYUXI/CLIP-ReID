efabe6b2462ea133beabe54fa161a69617c42ff2
yuchenyuxi <60705248+YUCHENYUXI@users.noreply.github.com>
2025-03-30 16:33:09 +0800

    com

--- processor/processor.py
+++ processor/processor.py
@@ -19,7 +19,7 @@ def do_train(cfg,
              loss_fn,
              num_query, 
              local_rank):
-    log_period = cfg.SOLVER.LOG_PERIOD
+    log_period = cfg.SOLVER.LOG_PERIOD # 初始化日志 检查点 评估周期 设备 epoch 日志 loss metric
     checkpoint_period = cfg.SOLVER.CHECKPOINT_PERIOD
     eval_period = cfg.SOLVER.EVAL_PERIOD
 
@@ -41,65 +41,65 @@ def do_train(cfg,
     evaluator = R1_mAP_eval(num_query, max_rank=50, feat_norm=cfg.TEST.FEAT_NORM)
     scaler = amp.GradScaler()
     
-    # train
+    #%% train  日志
     import time
     from datetime import timedelta
     all_start_time = time.monotonic()
     logger.info("model: {}".format(model))
-
+    #%% 训练周期
     for epoch in range(1, epochs + 1):
-        start_time = time.time()
+        start_time = time.time() # 打日志 初始化
         loss_meter.reset()
         acc_meter.reset()
         evaluator.reset()
 
         scheduler.step()
 
-        model.train()
-        for n_iter, (img, vid, target_cam, target_view) in enumerate(train_loader):
+        model.train()# 开始训练
+        for n_iter, (img, vid, target_cam, target_view) in enumerate(train_loader): # 取出一个minibatch
             optimizer.zero_grad()
-            optimizer_center.zero_grad()
-            img = img.to(device)
+            optimizer_center.zero_grad()# 优化器梯度清零
+            img = img.to(device)# 数据处理
             target = vid.to(device)
-            if cfg.MODEL.SIE_CAMERA:
+            if cfg.MODEL.SIE_CAMERA: # 相机标签处理
                 target_cam = target_cam.to(device)
             else: 
                 target_cam = None
-            if cfg.MODEL.SIE_VIEW:
+            if cfg.MODEL.SIE_VIEW:# 视角标签处理
                 target_view = target_view.to(device)
             else: 
                 target_view = None
-            with amp.autocast(enabled=True):
-                score, feat = model(img, target, cam_label=target_cam, view_label=target_view)
-                loss = loss_fn(score, feat, target, target_cam)
+            with amp.autocast(enabled=True):# 混合精度训练
+                score, feat = model(img, target, cam_label=target_cam, view_label=target_view) # 前向传播
+                loss = loss_fn(score, feat, target, target_cam)# 计算损失
+            #%%
+            scaler.scale(loss).backward()# 反向传播
 
-            scaler.scale(loss).backward()
-
-            scaler.step(optimizer)
-            scaler.update()
-
-            if 'center' in cfg.MODEL.METRIC_LOSS_TYPE:
+            scaler.step(optimizer)# 更新参数
+            scaler.update()# 更新缩放因子
+            #%%
+            if 'center' in cfg.MODEL.METRIC_LOSS_TYPE: # 更新中心损失
                 for param in center_criterion.parameters():
                     param.grad.data *= (1. / cfg.SOLVER.CENTER_LOSS_WEIGHT)
                 scaler.step(optimizer_center)
                 scaler.update()
-            if isinstance(score, list):
+            if isinstance(score, list):# 计算准确率
                 acc = (score[0].max(1)[1] == target).float().mean()
             else:
                 acc = (score.max(1)[1] == target).float().mean()
-
+            # 
             loss_meter.update(loss.item(), img.shape[0])
             acc_meter.update(acc, 1)
-
-            torch.cuda.synchronize()
+            #%%
+            torch.cuda.synchronize()# 同步
             if (n_iter + 1) % log_period == 0:
                 logger.info("Epoch[{}] Iteration[{}/{}] Loss: {:.3f}, Acc: {:.3f}, Base Lr: {:.2e}"
                             .format(epoch, (n_iter + 1), len(train_loader),
                                     loss_meter.avg, acc_meter.avg, scheduler.get_lr()[0]))
-
-        end_time = time.time()
+#%% 一个minibatch训练结束
+        end_time = time.time()# 仅仅是minibatch结束，不是epoch结束
         time_per_batch = (end_time - start_time) / (n_iter + 1)
-        if cfg.MODEL.DIST_TRAIN:
+        if cfg.MODEL.DIST_TRAIN:# 分布式训练用结束
             pass
         else:
             logger.info("Epoch {} done. Time per batch: {:.3f}[s] Speed: {:.1f}[samples/s]"
@@ -114,8 +114,8 @@ def do_train(cfg,
                 torch.save(model.state_dict(),
                            os.path.join(cfg.OUTPUT_DIR, cfg.MODEL.NAME + '_{}.pth'.format(epoch)))
 
-        if epoch % eval_period == 0:
-            if cfg.MODEL.DIST_TRAIN:
+        if epoch % eval_period == 0:# 当前epoch是评估周期的整数倍时，进行评估
+            if cfg.MODEL.DIST_TRAIN:# 分布式训练用
                 if dist.get_rank() == 0:
                     model.eval()
                     for n_iter, (img, vid, camid, camids, target_view, _) in enumerate(val_loader):
@@ -137,7 +137,7 @@ def do_train(cfg,
                     for r in [1, 5, 10]:
                         logger.info("CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
                     torch.cuda.empty_cache()
-            else:
+            else:# 单卡模式
                 model.eval()
                 for n_iter, (img, vid, camid, camids, target_view, _) in enumerate(val_loader):
                     with torch.no_grad():
@@ -157,12 +157,169 @@ def do_train(cfg,
                 logger.info("mAP: {:.1%}".format(mAP))
                 for r in [1, 5, 10]:
                     logger.info("CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
-                torch.cuda.empty_cache()
-
+                torch.cuda.empty_cache()# 一个epoch结束
+# 总的训练结束
     all_end_time = time.monotonic()
     total_time = timedelta(seconds=all_end_time - all_start_time)
     logger.info("Total running time: {}".format(total_time))
     print(cfg.OUTPUT_DIR)
+
+def do_train_vid(cfg,
+             model,
+             center_criterion,
+             train_loader,
+             val_loader,
+             optimizer,
+             optimizer_center,
+             scheduler,
+             loss_fn,
+             num_query, 
+             local_rank):
+    log_period = cfg.SOLVER.LOG_PERIOD # 初始化日志 检查点 评估周期 设备 epoch 日志 loss metric
+    checkpoint_period = cfg.SOLVER.CHECKPOINT_PERIOD
+    eval_period = cfg.SOLVER.EVAL_PERIOD
+
+    device = "cuda"
+    epochs = cfg.SOLVER.MAX_EPOCHS
+
+    logger = logging.getLogger("transreid.train")
+    logger.info('start training')
+    _LOCAL_PROCESS_GROUP = None
+    if device:
+        model.to(local_rank)
+        if torch.cuda.device_count() > 1:
+            print('Using {} GPUs for training'.format(torch.cuda.device_count()))
+            model = nn.DataParallel(model)  
+
+    loss_meter = AverageMeter()
+    acc_meter = AverageMeter()
+
+    evaluator = R1_mAP_eval(num_query, max_rank=50, feat_norm=cfg.TEST.FEAT_NORM)
+    scaler = amp.GradScaler()
+    
+    #%% train  日志
+    import time
+    from datetime import timedelta
+    all_start_time = time.monotonic()
+    logger.info("model: {}".format(model))
+    #%% 训练周期
+    for epoch in range(1, epochs + 1):
+        start_time = time.time() # 打日志 初始化
+        loss_meter.reset()
+        acc_meter.reset()
+        evaluator.reset()
+
+        scheduler.step()
+
+        model.train()# 开始训练
+        for n_iter, (img, vid, target_cam, target_view) in enumerate(train_loader): # 取出一个minibatch
+            optimizer.zero_grad()
+            optimizer_center.zero_grad()# 优化器梯度清零
+            img = img.to(device)# 数据处理
+            target = vid.to(device)
+            if cfg.MODEL.SIE_CAMERA: # 相机标签处理
+                target_cam = target_cam.to(device)
+            else: 
+                target_cam = None
+            if cfg.MODEL.SIE_VIEW:# 视角标签处理
+                target_view = target_view.to(device)
+            else: 
+                target_view = None
+            with amp.autocast(enabled=True):# 混合精度训练
+                score, feat = model(img, target, cam_label=target_cam, view_label=target_view) # 前向传播
+                loss = loss_fn(score, feat, target, target_cam)# 计算损失
+            #%%
+            scaler.scale(loss).backward()# 反向传播
+
+            scaler.step(optimizer)# 更新参数
+            scaler.update()# 更新缩放因子
+            #%%
+            if 'center' in cfg.MODEL.METRIC_LOSS_TYPE: # 更新中心损失
+                for param in center_criterion.parameters():
+                    param.grad.data *= (1. / cfg.SOLVER.CENTER_LOSS_WEIGHT)
+                scaler.step(optimizer_center)
+                scaler.update()
+            if isinstance(score, list):# 计算准确率
+                acc = (score[0].max(1)[1] == target).float().mean()
+            else:
+                acc = (score.max(1)[1] == target).float().mean()
+            # 
+            loss_meter.update(loss.item(), img.shape[0])
+            acc_meter.update(acc, 1)
+            #%%
+            torch.cuda.synchronize()# 同步
+            if (n_iter + 1) % log_period == 0:
+                logger.info("Epoch[{}] Iteration[{}/{}] Loss: {:.3f}, Acc: {:.3f}, Base Lr: {:.2e}"
+                            .format(epoch, (n_iter + 1), len(train_loader),
+                                    loss_meter.avg, acc_meter.avg, scheduler.get_lr()[0]))
+#%% 一个minibatch训练结束
+        end_time = time.time()# 仅仅是minibatch结束，不是epoch结束
+        time_per_batch = (end_time - start_time) / (n_iter + 1)
+        if cfg.MODEL.DIST_TRAIN:# 分布式训练用结束
+            pass
+        else:
+            logger.info("Epoch {} done. Time per batch: {:.3f}[s] Speed: {:.1f}[samples/s]"
+                    .format(epoch, time_per_batch, train_loader.batch_size / time_per_batch))
+
+        if epoch % checkpoint_period == 0:
+            if cfg.MODEL.DIST_TRAIN:
+                if dist.get_rank() == 0:
+                    torch.save(model.state_dict(),
+                               os.path.join(cfg.OUTPUT_DIR, cfg.MODEL.NAME + '_{}.pth'.format(epoch)))
+            else:
+                torch.save(model.state_dict(),
+                           os.path.join(cfg.OUTPUT_DIR, cfg.MODEL.NAME + '_{}.pth'.format(epoch)))
+
+        if epoch % eval_period == 0:# 当前epoch是评估周期的整数倍时，进行评估
+            if cfg.MODEL.DIST_TRAIN:# 分布式训练用
+                if dist.get_rank() == 0:
+                    model.eval()
+                    for n_iter, (img, vid, camid, camids, target_view, _) in enumerate(val_loader):
+                        with torch.no_grad():
+                            img = img.to(device)
+                            if cfg.MODEL.SIE_CAMERA:
+                                camids = camids.to(device)
+                            else: 
+                                camids = None
+                            if cfg.MODEL.SIE_VIEW:
+                                target_view = target_view.to(device)
+                            else: 
+                                target_view = None
+                            feat = model(img, cam_label=camids, view_label=target_view)
+                            evaluator.update((feat, vid, camid))
+                    cmc, mAP, _, _, _, _, _ = evaluator.compute()
+                    logger.info("Validation Results - Epoch: {}".format(epoch))
+                    logger.info("mAP: {:.1%}".format(mAP))
+                    for r in [1, 5, 10]:
+                        logger.info("CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
+                    torch.cuda.empty_cache()
+            else:# 单卡模式
+                model.eval()
+                for n_iter, (img, vid, camid, camids, target_view, _) in enumerate(val_loader):
+                    with torch.no_grad():
+                        img = img.to(device)
+                        if cfg.MODEL.SIE_CAMERA:
+                            camids = camids.to(device)
+                        else: 
+                            camids = None
+                        if cfg.MODEL.SIE_VIEW:
+                            target_view = target_view.to(device)
+                        else: 
+                            target_view = None
+                        feat = model(img, cam_label=camids, view_label=target_view)
+                        evaluator.update((feat, vid, camid))
+                cmc, mAP, _, _, _, _, _ = evaluator.compute()
+                logger.info("Validation Results - Epoch: {}".format(epoch))
+                logger.info("mAP: {:.1%}".format(mAP))
+                for r in [1, 5, 10]:
+                    logger.info("CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
+                torch.cuda.empty_cache()# 一个epoch结束
+# 总的训练结束
+    all_end_time = time.monotonic()
+    total_time = timedelta(seconds=all_end_time - all_start_time)
+    logger.info("Total running time: {}".format(total_time))
+    print(cfg.OUTPUT_DIR)
+
 
 def do_inference(cfg,
                  model,
