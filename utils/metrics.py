d2f82c22abbf3749392f9476f4246e747d3075ee
YUCHENYUXI <60705248+YUCHENYUXI@users.noreply.github.com>
2025-04-15 12:47:06 +0800

    metric还原

--- utils/metrics.py
+++ utils/metrics.py
@@ -3,71 +3,59 @@ import numpy as np
 from utils.reranking import re_ranking
 
 
-def euclidean_distance(qf, gf):
-    m = qf.shape[0]
-    n = gf.shape[0]
-    dist_mat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
-               torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
-    dist_mat.addmm_(1, -2, qf, gf.t())
-    return dist_mat.cpu().numpy()
-
-def cosine_similarity(qf, gf):
-    epsilon = 0.00001
-    dist_mat = qf.mm(gf.t())
-    qf_norm = torch.norm(qf, p=2, dim=1, keepdim=True)  # mx1
-    gf_norm = torch.norm(gf, p=2, dim=1, keepdim=True)  # nx1
-    qg_normdot = qf_norm.mm(gf_norm.t())
-
-    dist_mat = dist_mat.mul(1 / qg_normdot).cpu().numpy()
-    dist_mat = np.clip(dist_mat, -1 + epsilon, 1 - epsilon)
-    dist_mat = np.arccos(dist_mat)
-    return dist_mat
-
-
 def eval_func(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=50):
     """Evaluation with market1501 metric
-    Key: for each query identity, its gallery images from the same camera view are discarded.
-    """
+        Key: for each query identity, its gallery images from the same camera view are discarded.
+        """
     num_q, num_g = distmat.shape
 
     if num_g < max_rank:
         max_rank = num_g
         print("Note: number of gallery samples is quite small, got {}".format(num_g))
-
+        
     indices = np.argsort(distmat, axis=1)
+    #  0 2 1 3
+    #  1 2 3 0
     matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
-
+    # compute cmc curve for each query
     all_cmc = []
     all_AP = []
-    num_valid_q = 0  # number of valid query
-
+    num_valid_q = 0.  # number of valid query
     for q_idx in range(num_q):
+        # get query pid and camid
         q_pid = q_pids[q_idx]
         q_camid = q_camids[q_idx]
 
-        order = indices[q_idx]
+        # remove gallery samples that have the same pid and camid with query
+        order = indices[q_idx]  # select one row
         remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
         keep = np.invert(remove)
 
+        # compute cmc curve
+        # binary vector, positions with value 1 are correct matches
         orig_cmc = matches[q_idx][keep]
         if not np.any(orig_cmc):
+            # this condition is true when query identity does not appear in gallery
             continue
 
         cmc = orig_cmc.cumsum()
         cmc[cmc > 1] = 1
-        all_cmc.append(cmc[:max_rank])
-        num_valid_q += 1
 
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
         num_rel = orig_cmc.sum()
         tmp_cmc = orig_cmc.cumsum()
-        precision = tmp_cmc / (np.arange(1, tmp_cmc.shape[0] + 1).astype(np.float32))
-        AP = (precision * orig_cmc).sum() / num_rel
+        #tmp_cmc = [x / (i + 1.) for i, x in enumerate(tmp_cmc)]
+        y = np.arange(1, tmp_cmc.shape[0] + 1) * 1.0
+        tmp_cmc = tmp_cmc / y
+        tmp_cmc = np.asarray(tmp_cmc) * orig_cmc
+        AP = tmp_cmc.sum() / num_rel
         all_AP.append(AP)
 
-    if num_valid_q == 0:
-        print("Warning: all query identities do not appear in gallery or are filtered out by camera ID.")
-        empty_result = np.zeros(max_rank, dtype=np.float32)
-        return empty_result, 0.0
+    assert num_valid_q > 0, "Error: all query identities do not appear in gallery"
 
     all_cmc = np.asarray(all_cmc).astype(np.float32)
     all_cmc = all_cmc.sum(0) / num_valid_q
@@ -93,8 +81,7 @@ class R1_mAP_eval():
         feat, pid, camid = output
         self.feats.append(feat.cpu())
         self.pids.extend(np.asarray(pid))
-        if camid is not None:
-            self.camids.extend(np.asarray(camid))
+        self.camids.extend(np.asarray(camid))
 
     def compute(self):  # called after each epoch
         feats = torch.cat(self.feats, dim=0)
@@ -117,7 +104,7 @@ class R1_mAP_eval():
 
         else:
             print('=> Computing DistMat with euclidean_distance')
-            distmat = euclidean_distance(qf, gf)
+            distmat = torch.cdist(qf, gf).cpu().numpy()
         cmc, mAP = eval_func(distmat, q_pids, g_pids, q_camids, g_camids)
 
         return cmc, mAP, distmat, self.pids, self.camids, qf, gf
