932acfd1954d75d7da03c441c66343d16b5393f4
YUCHENYUXI <60705248+YUCHENYUXI@users.noreply.github.com.>
2025-05-19 22:35:38 +0800

    dataloader

--- datasets/make_dataloader_vid.py
+++ datasets/make_dataloader_vid.py
@@ -1,11 +1,20 @@
 import datasets.vid_utils.transforms.spatial_transforms as ST 
 import datasets.vid_utils.transforms.temporal_transforms as TT
 from torch.utils.data import DataLoader
-from .vid_utils.tools.video_loader import VideoDataset,AERDataset
+from .vid_utils.tools.video_loader import AERDataset
 from .vid_utils.tools.samplers import RandomIdentitySampler
 import  datasets.vid_utils.tools.data_manager as data_manager
 
 def make_dataloader(cfg):
+    """
+    returns:
+        train_loader: DataLoader for training
+        val_loader: DataLoader for validation
+        num_query: number of query samples
+        num_classes: number of classes
+        camera_num: number of cameras
+        view_num: number of views
+    """
     # Data augmentation
     # train
     spatial_transform_train =ST.Compose([ # 训练集-空间
@@ -32,23 +41,18 @@ def make_dataloader(cfg):
     # dataset
     dataset=data_manager.init_dataset(name=cfg.DATASETS.NAMES, root=cfg.DATASETS.ROOT_DIR)
     cam_num=len(set(dataset.cam_ids))
-    if False and cfg.DATASETS.NAMES == "aer_video":
-        dataset_event = data_manager.init_dataset(name=cfg.DATASETS.NAMES, root=cfg.DATASETS.ROOT_DIR,modal='event')
-        dataset_rgb = data_manager.init_dataset(name=cfg.DATASETS.NAMES, root=cfg.DATASETS.ROOT_DIR,modal='rgb')
-        cam_num=len(set(dataset_rgb.cam_ids) | set(dataset_event.cam_ids) )
-    #
     is_pin = True
     res ={"train":DataLoader(
                     AERDataset(
                         dataset.train, 
-                        spatial_transform=spatial_transform_train, 
-                        temporal_transform=temporal_transform_train
+                        spatial_transform=spatial_transform_train, #
+                        temporal_transform=temporal_transform_train#
                         ),
                     sampler=RandomIdentitySampler(
                         dataset.train, 
                         num_instances=cfg.DATALOADER.NUM_INSTANCE
                         ),
-                    batch_size=cfg.SOLVER.IMS_PER_BATCH, 
+                    batch_size=cfg.SOLVER.VIDS_PER_BATCH, 
                     num_workers=cfg.DATALOADER.NUM_WORKERS,
                     pin_memory=is_pin, 
                     drop_last=True,
@@ -59,7 +63,7 @@ def make_dataloader(cfg):
                             spatial_transform=spatial_transform_test, 
                             temporal_transform=temporal_transform_test
                             ),
-                batch_size=cfg.TEST.IMS_PER_BATCH, 
+                batch_size=cfg.TEST.VIDS_PER_BATCH, 
                 shuffle=False, 
                 num_workers=cfg.DATALOADER.NUM_WORKERS,
                 pin_memory=is_pin, 
@@ -72,69 +76,3 @@ def make_dataloader(cfg):
           "view_num": None
           }
     return res
-    # is_pin = True
-    # res ={"train":
-    #         {'event':DataLoader(
-    #                 VideoDataset(
-    #                     dataset_event.train, 
-    #                     spatial_transform=spatial_transform_train, 
-    #                     temporal_transform=temporal_transform_train
-    #                     ),
-    #                 sampler=RandomIdentitySampler(
-    #                     dataset_event.train, 
-    #                     num_instances=cfg.DATALOADER.NUM_INSTANCE
-    #                     ),
-    #                 batch_size=cfg.SOLVER.IMS_PER_BATCH, 
-    #                 num_workers=cfg.DATALOADER.NUM_WORKERS,
-    #                 pin_memory=is_pin, 
-    #                 drop_last=True,
-    #                 persistent_workers=True,
-    #             ),
-    #         'rgb':DataLoader(
-    #                 VideoDataset(
-    #                     dataset_rgb.train, 
-    #                     spatial_transform=spatial_transform_train, 
-    #                     temporal_transform=temporal_transform_train
-    #                     ),
-    #                 sampler=RandomIdentitySampler(
-    #                     dataset_rgb.train, 
-    #                     num_instances=cfg.DATALOADER.NUM_INSTANCE
-    #                     ),
-    #                 batch_size=cfg.SOLVER.IMS_PER_BATCH, 
-    #                 num_workers=cfg.DATALOADER.NUM_WORKERS,
-    #                 pin_memory=is_pin, 
-    #                 drop_last=True,
-    #                 persistent_workers=True,
-    #             )
-    #         },
-    #       "val":{ 'event':DataLoader(
-    #             VideoDataset(dataset_event.query + dataset_event.gallery, 
-    #                         spatial_transform=spatial_transform_test, 
-    #                         temporal_transform=temporal_transform_test
-    #                         ),
-    #             batch_size=cfg.TEST.IMS_PER_BATCH, 
-    #             shuffle=False, 
-    #             num_workers=cfg.DATALOADER.NUM_WORKERS,
-    #             pin_memory=is_pin, 
-    #             drop_last=True,
-    #             persistent_workers=True,
-    #         ),
-    #         'rgb':DataLoader(
-    #             VideoDataset(dataset_rgb.query + dataset_rgb.gallery, 
-    #                         spatial_transform=spatial_transform_test, 
-    #                         temporal_transform=temporal_transform_test
-    #                         ),
-    #             batch_size=cfg.TEST.IMS_PER_BATCH, 
-    #             shuffle=False, 
-    #             num_workers=cfg.DATALOADER.NUM_WORKERS,
-    #             pin_memory=is_pin, 
-    #             drop_last=True,
-    #             persistent_workers=True,
-    #         ) 
-    #         },
-    #       "query_num":len(dataset.query),
-    #       "cls_num": dataset.num_train_pids,
-    #       "cam_num": cam_num,
-    #       "view_num": None
-    #       }
-    # return res
--- datasets/vid_utils/tools/video_loader.py
+++ datasets/vid_utils/tools/video_loader.py
@@ -55,6 +55,7 @@ class VideoDataset(data.Dataset):
         clip = torch.stack(clip, 0) # .permute(1, 0, 2, 3)
 
         return clip, pid, camid
+
 class AERDataset(data.Dataset):
     """RGB-AER Person ReID Dataset.
     Note:
@@ -79,14 +80,16 @@ class AERDataset(data.Dataset):
         Args:
             index (int): Index
         Returns:
-            tuple: (rgb, aer, pid, camid) where pid is identity of the clip.
+        dict——
+            'aer': aer,
+            'rgb': rgb,
+            'pid': pid,
+            'camid': camid
         """
         img_paths, pid, camid = self.dataset[index]
-
         if self.temporal_transform is not None:
             img_paths = self.temporal_transform(img_paths)
-
-        # load video
+        # load video # replace
         aer_path=[]
         rgb_path=[]
         aer = self.loader(img_paths)
@@ -96,41 +99,13 @@ class AERDataset(data.Dataset):
             self.spatial_transform.randomize_parameters()
             aer = [self.spatial_transform(img) for img in aer]
             rgb = [self.spatial_transform(img) for img in rgb]
-
         # trans T x C x H x W to C x T x H x W
         aer = torch.stack(aer, 0)
         rgb = torch.stack(rgb, 0)
-
-        return aer, rgb, pid, camid
-
-class ImageDataset(data.Dataset):
-    """Video Person ReID Dataset.
-    Note:
-        Batch data has shape N x C x H x W
-    Args:
-        dataset (list): List with items (img_paths, pid, camid)
-        transform (callable, optional): A function/transform that  takes in the
-            imgs and transforms it.
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        loader (callable, optional): A function to load an video given its path and frame indices.
-    """
-
-    def __init__(self, dataset, transform=None):
-        self.dataset = dataset
-        self.transform = transform
-    def __len__(self):
-        return len(self.dataset)
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-        Returns:
-            tuple: (img, pid, camid) where pid is identity of the clip.
-        """
-        img_path, pid, camid = self.dataset[index]
-        img = Image.open(img_path)
-        if self.transform is not None:
-            img = self.transform(img)
-
-        return img, pid, camid
+        # return aer, rgb, pid, camid
+        return {
+            'aer': aer,
+            'rgb': rgb,
+            'pid': pid,
+            'camid': camid
+        }
